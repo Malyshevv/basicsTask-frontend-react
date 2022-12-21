86920106d6e199c9988b26319adcc802ccbc888f
Белецкий Андрей Анатольевич <abeletskiy@corp.local>
2022-12-21 16:28:39 +0200

    добавил навигацию в главном меню

--- src/shared/Main/Main.tsx
+++ src/shared/Main/Main.tsx
@@ -4,6 +4,8 @@ import './main.css';
 import {useSelector} from "react-redux";
 import {RootState} from "../../utils/redux/reducer";
 import {MapHome} from '../MapHome'
+import {Link} from "react-router-dom";
+import {MainLogo} from "../../assets/svg/MainLogo";
 
 export function Main() {
 
@@ -18,12 +20,68 @@ export function Main() {
         }
     }, [token]);
 
-  return (
+    const [isVisible, setIsVisible] = useState(true);
+    const toggleMenu = () => setIsVisible((isVisible) => !isVisible);
+
+
+    return (
       <div className="Main__container">
           {/*<h1>Main Page</h1> <br/>*/}
           {/*<p><label>User Type:</label> {userType}</p>*/}
-          <div style={{width: "20%", height: '100vh', zIndex: '1001'}} />
-          <MapHome />
+
+          <div  style={{position: 'relative'}}>
+              {isVisible ? (
+                      <div className="Main-menu__container">
+                          <div className="Main-menu__logo_container">
+                              <div className="Main-menu__logo">
+                                  <Link to="/signup">
+                                      <MainLogo />
+                                  </Link>
+                              </div>
+                          </div>
+
+                          <nav className="Navigation__container">
+                              <Link className="Navigation__link" to="">Главная страница</Link>
+                              <Link className="Navigation__link" to="/signup">Трансляция видео</Link>
+                              <Link className="Navigation__link-paragraph" to="">Пассажиропоток</Link>
+                              <Link className="Navigation__link-subparagraph" to="">по транспорту</Link>
+                              <Link className="Navigation__link-subparagraph" to="">по маршруту</Link>
+                              <Link className="Navigation__link-paragraph" to="">Качество вождения</Link>
+                              <Link className="Navigation__link-subparagraph" to="">рейтинг водителей</Link>
+                              <Link className="Navigation__link-subparagraph" to="">водители</Link>
+                              <Link className="Navigation__link-subparagraph" to="">филиалы</Link>
+                              <Link className="Navigation__link-subparagraph" to="">транспорт</Link>
+                              <Link className="Navigation__link" to="">Управление медиапанелью</Link>
+                              <Link className="Navigation__link" to="">Административная панель</Link>
+                          </nav>
+                      </div>
+              ): null}
+
+              <div className={isVisible ? "Main-menu__button-open" : "Main-menu__button-close" } onClick={toggleMenu}>
+                  {isVisible ?
+                      (
+                         <div style={{transform: 'rotate(180deg)', width: '15px',}}>
+                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 11.25 19.74"><title>arrow small</title>
+                                 <path d="M54.85,32.65l-8.49-8.49a1.38,1.38,0,0,0-2,2l7.51,7.51L44.4,41.14a1.38,1.38,0,0,0,2,2l8.49-8.49a1.38,1.38,0,0,0,0-2Z"
+                                    transform="translate(-44 -23.76)"></path>
+                             </svg>
+                         </div>
+                      ) :
+                      (
+                          <div style={{width: '15px'}}>
+                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 11.25 19.74"><title>arrow small</title>
+                                  <path d="M54.85,32.65l-8.49-8.49a1.38,1.38,0,0,0-2,2l7.51,7.51L44.4,41.14a1.38,1.38,0,0,0,2,2l8.49-8.49a1.38,1.38,0,0,0,0-2Z"
+                                        transform="translate(-44 -23.76)"></path>
+                              </svg>
+                          </div>
+                      )
+                  }
+
+              </div>
+          </div>
+
+              <MapHome />
+
       </div>
   );
 }
--- src/shared/MapHome/MapHome.tsx
+++ src/shared/MapHome/MapHome.tsx
@@ -1,22 +1,24 @@
 import React from "react";
 import L from 'leaflet';
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import {MapContainer, TileLayer, Marker, Popup, ZoomControl} from "react-leaflet";
 import './mapHome.css';
-// import 'leaflet/dist/leaflet.css';
 
 // указываем путь к файлам marker
 L.Icon.Default.imagePath = "https://unpkg.com/leaflet@1.5.0/dist/images/";
 
 export function MapHome() {
     return (
-        <MapContainer center={[55.74668, 37.613960]} zoom={11} scrollWheelZoom={false}>
+        <MapContainer center={[55.74668, 37.613960]} zoom={11} scrollWheelZoom={true} zoomControl={false}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[55.74668, 37.613960]}>
+            <ZoomControl position='topright' />
+            <Marker position={[55.75088, 37.618960]}>
                 <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
+                    Здесь работает
+                    <br/>
+                    В.В.Путин
                 </Popup>
             </Marker>
         </MapContainer>
