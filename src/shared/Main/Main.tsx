944c9e787fa626b6b9265a88bf93e15224b491ab
Белецкий Андрей Анатольевич <abeletskiy@corp.local>
2022-12-23 20:10:59 +0200

    главное меню - стилизация 2 этап

--- src/shared/Main/Main.tsx
+++ src/shared/Main/Main.tsx
@@ -11,7 +11,8 @@ import {BusLogo} from "../../assets/svg/BusLogo";
 import {TramLogo} from "../../assets/svg/TramLogo";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {
-    faChartLine,
+    faBars, faBarsProgress, faBarsStaggered, faBus, faBusAlt, faBusSimple,
+    faChartLine, faEllipsis, faEllipsisH, faEllipsisV, faEllipsisVertical,
     faGear,
     faHouse,
     faPersonMilitaryToPerson,
@@ -19,6 +20,8 @@ import {
     faTablet,
     faVideo
 } from '@fortawesome/free-solid-svg-icons'
+import {SelectLabel} from "../../components/SelectLabel";
+import {InputLabel} from "../../components/InputLabel";
 
 
 export function Main() {
@@ -43,7 +46,11 @@ export function Main() {
     const toggleMenuPassengerTraffic = () => setShowPassengerTraffic((showPassengerTraffic) => !showPassengerTraffic);
     const [showMenuRoute, setShowMenuRoute] = useState(false);
     const toggleMenuRoute = () => setShowMenuRoute((showMenuRoute) => !showMenuRoute);
+    const [showMenuVehicles, setShowMenuVehicles] = useState(false);
+    const toggleMenuVehicles = () => setShowMenuVehicles((showMenuVehicles) => !showMenuVehicles);
 
+
+    const v = ['1', '2', '3', '4']
 
     return (
       <div className="Main__container">
@@ -62,19 +69,19 @@ export function Main() {
                       <nav className="Navigation__container">
                           <div className='Navigation__link_container--border'>
                               <Link className='Navigation__link_container' to="">
-                                  <FontAwesomeIcon className="Font-Icon" icon={faHouse} />
+                                  <FontAwesomeIcon className="Navigation__Font-Icon" icon={faHouse} />
                                   <h3 className="Navigation__link" >Главная страница</h3>
                               </Link>
                           </div>
                           <div className='Navigation__link_container--border'>
                               <Link className="Navigation__link_container" to="/signup">
-                                  <FontAwesomeIcon className="Font-Icon" icon={faVideo} />
+                                  <FontAwesomeIcon className="Navigation__Font-Icon" icon={faVideo} />
                                   <h3 className="Navigation__link">Трансляция видео</h3>
                               </Link>
                           </div>
                           <div className='Navigation__link_container--border' onClick={toggleMenuPassengerTraffic}>
                               <Link className="Navigation__link_container" to="">
-                                  <FontAwesomeIcon className="Font-Icon" icon={faPersonMilitaryToPerson} />
+                                  <FontAwesomeIcon className="Navigation__Font-Icon" icon={faPersonMilitaryToPerson} />
                                   <h3 className="Navigation__link">Пассажиропоток</h3>
                               </Link>
                           </div>
@@ -90,7 +97,7 @@ export function Main() {
                           }
                           <div className='Navigation__link_container--border' onClick={toggleMenuDrivingQuality}>
                               <Link className="Navigation__link_container" to="">
-                                  <FontAwesomeIcon className="Font-Icon" icon={faChartLine} />
+                                  <FontAwesomeIcon className="Navigation__Font-Icon" icon={faChartLine} />
                                   <h3 className="Navigation__link">Качество вождения</h3>
                               </Link>
 
@@ -114,62 +121,208 @@ export function Main() {
 
                           <div className='Navigation__link_container--border'>
                               <Link className="Navigation__link_container" to="">
-                                  <FontAwesomeIcon className="Font-Icon" icon={faTablet} />
+                                  <FontAwesomeIcon className="Navigation__Font-Icon" icon={faTablet} />
                                   <h3 className="Navigation__link">Управление медиапанелью</h3>
                               </Link>
                           </div>
                           <div className='Navigation__link_container--border'>
                               <Link className="Navigation__link_container" to="">
-                                  <FontAwesomeIcon className="Font-Icon" icon={faGear} />
+                                  <FontAwesomeIcon className="Navigation__Font-Icon" icon={faGear} />
                                   <h3 className="Navigation__link">Административная панель</h3>
                               </Link>
                           </div>
                       </nav>
 
-                      <div style={{display: 'flex', justifyContent: 'flex-end', paddingTop: '30px'}}>
-                          <FontAwesomeIcon className="Font-Icon__button" icon={faSliders} onClick={toggleMenuRoute} />
+                      <div className="Burger-Icon__main-container">
+                          <div className="Burger-Icon__background-container">
+                             <FontAwesomeIcon className={"Burger-Icon__button"}  icon={faBarsStaggered} onClick={toggleMenuRoute} />
+                          </div>
                       </div>
 
-
-                      <div style={{paddingTop: '40px'}}>
+                      <div style={{paddingTop: '240px'}}>
                           <h2 className="Main__tagline">Умный</h2>
                           <h2 className="Main__tagline">транспорт</h2>
                       </div>
 
 
-                      <div style={{display: 'flex', alignItems: 'center', paddingTop: '15px', justifyContent: 'space-around'}}>
-                          <div style={{width: '45px', fill: '#017bb9'}}>
+                      <div style={{display: 'flex', alignItems: 'center', paddingTop: '5px', justifyContent: 'center', opacity: '0.5'}}>
+                          <div style={{width: '25px', fill: '#017bb9'}}>
                               <TrolleyBusLogo/>
                           </div>
-                          <div style={{width: '45px', fill: '#017bb9'}} >
+                          <div style={{width: '25px', fill: '#017bb9', marginLeft: "15px", marginRight: "15px"}} >
                               <BusLogo/>
                           </div>
-                          <div style={{width: '45px', fill: '#017bb9'}}>
+                          <div style={{width: '25px', fill: '#017bb9'}}>
                               <TramLogo/>
                           </div>
                       </div>
                   </div>
               }
               {!showMenu &&
-                  <div className="Main-menu__button-open" onMouseEnter={() => setShowMenu((!showMenu))}>
-                      <FontAwesomeIcon className="Font-Icon__button" icon={faHouse} />
-                      <FontAwesomeIcon className="Font-Icon__button" icon={faVideo} />
-                      <FontAwesomeIcon className="Font-Icon__button" icon={faPersonMilitaryToPerson} />
-                      <FontAwesomeIcon className="Font-Icon__button" icon={faChartLine} />
-                      <FontAwesomeIcon className="Font-Icon__button" icon={faTablet} />
-                      <FontAwesomeIcon className="Font-Icon__button" icon={faGear} />
+                  <div className="Main-menu__container-small" >
+                          <Link to="/signup" style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                              <div className="Main-menu__logo--small">
+                              <MainLogo />
+                              </div>
+                          </Link>
+                      <div className="Container-small__Font-Icon_container" onMouseEnter={() => setShowMenu((!showMenu))}>
+                          <FontAwesomeIcon className="Font-Icon__button" icon={faHouse} />
+                          <FontAwesomeIcon className="Font-Icon__button" icon={faVideo} />
+                          <FontAwesomeIcon className="Font-Icon__button" icon={faPersonMilitaryToPerson} />
+                          <FontAwesomeIcon className="Font-Icon__button" icon={faChartLine} />
+                          <FontAwesomeIcon className="Font-Icon__button" icon={faTablet} />
+                          <FontAwesomeIcon className="Font-Icon__button" icon={faGear} />
+                      </div>
+                      <div className="Container-small__Burger-Icon_container">
+                          <FontAwesomeIcon className={"Burger-Icon__button"}
+                                           icon={faBarsStaggered}
+                                           onClick={() => {toggleMenu()
+                                               toggleMenuRoute()
+                                           }} />
+                      </div>
                   </div>
               }
               {showMenuRoute &&
                   <div className="Main-menu__container--routes">
 
+                      <div style={{display: 'flex', flexFlow: 'column',}}>
+                          <p className="Vehicles_big-container__title">Маршруты транспортных средств</p>
+                          <div style={{margin: '10px'}} />
+                          <SelectLabel arr={v} name={'role'} label={'Поиск по номеру маршрута'}/>
+                              <div>
+                                  <div className="Vehicles__form-field">
+                                      <input type='text' placeholder=" " name='transport-find' />
+                                      <label>Поиск по ГРЗ/гаражному № <br/> минимум 3 символа</label>
+                                  </div>
+                              </div>
+                              <div className="Vehicles-checkbox__container">
+                                  <input className="Login__customCheckbox" type="checkbox" id="Online-transport"/>
+                                  <label className='Vehicles-checkbox__label' htmlFor="Online-transport">Онлайн</label>
+                              </div>
+                              <div style={{margin: '10px'}}/>
+                              <div className="Vehicles-checkbox__container">
+                                  <input className="Login__customCheckbox" type="checkbox" id="Actual-transport"/>
+                                  <label className='Vehicles-checkbox__label' htmlFor="Actual-transport">Актуальный пассажиропоток</label>
+                              </div>
+                              <div style={{margin: '35px'}}/>
+
+                      </div>
+
+
+
+
                   </div>
               }
 
 
-
           </div>
           <MapHome />
+
+
+
+          <div className="Vehicles_small-container" onClick={toggleMenuVehicles}>
+                <p className="Vehicles_small-container__text" style={{color: '#017bb9'}}>Всего: 2200</p>
+                  <FontAwesomeIcon  style={{color: '#017bb9', width: '20px', height: '20px'}} icon={faEllipsisVertical} onClick={toggleMenuVehicles} />
+          </div>
+          <div className="Vehicles_small-contain" onClick={toggleMenuVehicles}>
+              <p className="Vehicles_small-container__text">Онлайн: 1200</p>
+              <FontAwesomeIcon  style={{color: 'rgb(20 129 97)', width: '20px', height: '20px'}} icon={faEllipsisVertical}  />
+          </div>
+
+
+
+
+
+          {showMenuVehicles &&
+              <div className="Vehicles_big-container">
+                  <div className="Vehicles_table-contents__container">
+                      <span className="Vehicles_table-contents__text">Тип</span>
+                      <span className="Vehicles_table-contents__text">Гаражный</span>
+                      <span className="Vehicles_table-contents__text">Маршрут</span>
+                      <span className="Vehicles_table-contents__text">ГРЗ</span>
+                  </div>
+                  <div className="Vehicles-list__container">
+                      <FontAwesomeIcon className="Vehicles-list__Font-Icon" icon={faBusSimple} />
+                      <span className="Vehicles-list__text">191138</span>
+                      <span className="Vehicles-list__text">400T</span>
+                      <span className="Vehicles-list__text">K142УТ99</span>
+                  </div>
+                  <div className="Vehicles-list__container">
+                      <FontAwesomeIcon className="Vehicles-list__Font-Icon" icon={faBusSimple} />
+                      <span className="Vehicles-list__text">191138</span>
+                      <span className="Vehicles-list__text">400T</span>
+                      <span className="Vehicles-list__text">K142УТ99</span>
+                  </div>
+                  <div className="Vehicles-list__container">
+                      <FontAwesomeIcon className="Vehicles-list__Font-Icon" icon={faBusSimple} />
+                      <span className="Vehicles-list__text">191138</span>
+                      <span className="Vehicles-list__text">400T</span>
+                      <span className="Vehicles-list__text">K142УТ99</span>
+                  </div>
+                  <div className="Vehicles-list__container">
+                      <FontAwesomeIcon className="Vehicles-list__Font-Icon" icon={faBusSimple} />
+                      <span className="Vehicles-list__text">191138</span>
+                      <span className="Vehicles-list__text">400T</span>
+                      <span className="Vehicles-list__text">K142УТ99</span>
+                  </div>
+                  <div className="Vehicles-list__container">
+                      <FontAwesomeIcon className="Vehicles-list__Font-Icon" icon={faBusSimple} />
+                      <span className="Vehicles-list__text">191138</span>
+                      <span className="Vehicles-list__text">400T</span>
+                      <span className="Vehicles-list__text">K142УТ99</span>
+                  </div>
+                  <div className="Vehicles-list__container">
+                      <FontAwesomeIcon className="Vehicles-list__Font-Icon" icon={faBusSimple} />
+                      <span className="Vehicles-list__text">191138</span>
+                      <span className="Vehicles-list__text">400T</span>
+                      <span className="Vehicles-list__text">K142УТ99</span>
+                  </div>
+                  <div className="Vehicles-list__container">
+                      <FontAwesomeIcon className="Vehicles-list__Font-Icon" icon={faBusSimple} />
+                      <span className="Vehicles-list__text">191138</span>
+                      <span className="Vehicles-list__text">400T</span>
+                      <span className="Vehicles-list__text">K142УТ99</span>
+                  </div>
+                  <div className="Vehicles-list__container">
+                      <FontAwesomeIcon className="Vehicles-list__Font-Icon" icon={faBusSimple} />
+                      <span className="Vehicles-list__text">191138</span>
+                      <span className="Vehicles-list__text">400T</span>
+                      <span className="Vehicles-list__text">K142УТ99</span>
+                  </div>
+                  <div className="Vehicles-list__container">
+                      <FontAwesomeIcon className="Vehicles-list__Font-Icon" icon={faBusSimple} />
+                      <span className="Vehicles-list__text">191138</span>
+                      <span className="Vehicles-list__text">400T</span>
+                      <span className="Vehicles-list__text">K142УТ99</span>
+                  </div>
+                  <div className="Vehicles-list__container">
+                      <FontAwesomeIcon className="Vehicles-list__Font-Icon" icon={faBusSimple} />
+                      <span className="Vehicles-list__text">191138</span>
+                      <span className="Vehicles-list__text">400T</span>
+                      <span className="Vehicles-list__text">K142УТ99</span>
+                  </div>
+                  <div className="Vehicles-list__container">
+                      <FontAwesomeIcon className="Vehicles-list__Font-Icon" icon={faBusSimple} />
+                      <span className="Vehicles-list__text">191138</span>
+                      <span className="Vehicles-list__text">400T</span>
+                      <span className="Vehicles-list__text">K142УТ99</span>
+                  </div>
+                  <div className="Vehicles-list__container">
+                      <FontAwesomeIcon className="Vehicles-list__Font-Icon" icon={faBusSimple} />
+                      <span className="Vehicles-list__text">191138</span>
+                      <span className="Vehicles-list__text">400T</span>
+                      <span className="Vehicles-list__text">K142УТ99</span>
+                  </div>
+                  <div className="Vehicles-list__container">
+                      <FontAwesomeIcon className="Vehicles-list__Font-Icon" icon={faBusSimple} />
+                      <span className="Vehicles-list__text">191138</span>
+                      <span className="Vehicles-list__text">400T</span>
+                      <span className="Vehicles-list__text">K142УТ99</span>
+                  </div>
+
+              </div>
+
+          }
       </div>
     );
 }
